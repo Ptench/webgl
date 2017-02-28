0fadeca12153c9dad779b8f7a8ba06440f8b7321
Buylina Marina <buylina@gfcc.su>
2017-02-28 17:58:08 +0300

    grid

--- js/3d.js
+++ js/3d.js
@@ -3,14 +3,14 @@ window.addEventListener('load', function () {
   var camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
   var renderer = new THREE.WebGLRenderer();
 
-  camera.position.x = 200;
-				camera.position.y = 100;
-				camera.position.z = 200;
+  camera.position.x = 0;
+				camera.position.y = 200;
+				camera.position.z = 500;
 
   renderer.setSize( window.innerWidth -100, window.innerHeight - 100 );
   document.getElementById('world').appendChild(renderer.domElement);
 
-  var size = 500, step = 50;
+  var size = 300, step = 50;
 				var geometry = new THREE.Geometry();
 				for ( var i = - size; i <= size; i += step ) {
 					geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
@@ -18,7 +18,7 @@ window.addEventListener('load', function () {
 					geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
 					geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
 				}
-				var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2 } );
+				var material = new THREE.LineBasicMaterial( { color: 0xffffff} );
 				var line = new THREE.LineSegments( geometry, material );
 				scene.add( line );
   function render() {
