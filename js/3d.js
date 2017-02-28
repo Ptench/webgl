b31e255a7dd1262ecba52ce6c11ea9968440cbbd
Buylina Marina <buylina@gfcc.su>
2017-02-28 18:46:58 +0300

    grid

--- js/3d.js
+++ js/3d.js
@@ -21,6 +21,32 @@ window.addEventListener('load', function () {
 				var material = new THREE.LineBasicMaterial( { color: 0xffffff} );
 				var line = new THREE.LineSegments( geometry, material );
 				scene.add( line );
+
+        var geometry = new THREE.BoxGeometry( 50, 50, 50 );
+				var materiall = new THREE.MeshLambertMaterial( { color: 0x62CB31, overdraw: 0.5 } );
+				for ( var i = 0; i < 100; i ++ ) {
+					var cube = new THREE.Mesh( geometry, materiall );
+					cube.scale.y = Math.floor( Math.random() * 2 + 1 );
+					cube.position.x = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
+					cube.position.y = ( cube.scale.y * 50 ) / 2;
+					cube.position.z = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
+					scene.add( cube );
+				}
+        var ambientLight = new THREE.AmbientLight( Math.random() * 0x10 );
+      scene.add( ambientLight );
+      var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
+      directionalLight.position.x = Math.random() - 0.5;
+      directionalLight.position.y = Math.random() - 0.5;
+      directionalLight.position.z = Math.random() - 0.5;
+      directionalLight.position.normalize();
+      scene.add( directionalLight );
+      var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
+      directionalLight.position.x = Math.random() - 0.5;
+      directionalLight.position.y = Math.random() - 0.5;
+      directionalLight.position.z = Math.random() - 0.5;
+      directionalLight.position.normalize();
+      scene.add( directionalLight );
+
   function render() {
       requestAnimationFrame(render);
       renderer.render(scene, camera);
